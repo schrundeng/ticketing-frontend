fd1352484e41a50560f7c57426030c977b5a5133
Lwarentina <raditya.hanarta@gmail.com>
2024-10-17 09:42:19 +0700

    tables update

--- src/pages/dashboard/navbarPimpinan.js
+++ src/pages/dashboard/navbarPimpinan.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faEnvelope,
   faSignOutAlt,
   faChevronDown,
   faTicketAlt,
